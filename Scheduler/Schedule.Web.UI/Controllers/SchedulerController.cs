8be195ff72265cb585f057f061172f3a2f7c09f7
Matheus Alves <carvalho.matheus1997@gmail.com>
2022-06-28 21:47:37 -0300

    [FRONTEND] Scheduler innitial html

---
+++ Scheduler/Schedule.Web.UI/Controllers/SchedulerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Schedule.Web.UI.Controllers
+{
+    public class SchedulerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
