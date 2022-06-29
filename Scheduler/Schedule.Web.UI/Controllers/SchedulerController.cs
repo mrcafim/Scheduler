e0f68c346e5e59985b67d2d6965de1d3b4c069f4
Matheus Alves <carvalho.matheus1997@gmail.com>
2022-06-28 22:22:23 -0300

    [FRONTEND] Schedule dinamically assembled

--- Scheduler/Schedule.Web.UI/Controllers/SchedulerController.cs
+++ Scheduler/Schedule.Web.UI/Controllers/SchedulerController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Scheduler.Web.Application.Interfaces;
+using Scheduler.Web.Domain.Events.Queries;
 
 namespace Schedule.Web.UI.Controllers
 {
     public class SchedulerController : Controller
     {
-        public IActionResult Index()
+        private readonly IEventAppService _eventsAppService;
+        public SchedulerController(IEventAppService eventsAppService)
         {
-            return View();
+            _eventsAppService = eventsAppService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _eventsAppService.GetByDate(DateTime.Now));
+        }
+
+        [HttpGet("Get")]
+        public async Task<IEnumerable<EventQueryResult>> Get(DateTime date)
+        {
+            var events = await _eventsAppService.GetByDate(date);
+
+            return events;
         }
     }
 }
